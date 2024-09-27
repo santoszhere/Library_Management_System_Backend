c9e5991fd19b2c4c7b50b1ecbaf51428414a0445
=Prachanda Rana <=>
2024-09-27 17:37:48 +0545

    implement the review logic

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,25 +4,26 @@ export const PORT = process.env.PORT;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const DB_NAME = "LMS";
 export const cookieOptions = {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 5 * 24 * 60 * 60 * 1000,
 };
 
 export const ChatEventEnum = Object.freeze({
-    CONNECTED_EVENT: "connected",
-    DISCONNECT_EVENT: "disconnect",
-    JOIN_CHAT_EVENT: "joinChat",
-    LEAVE_CHAT_EVENT: "leaveChat",
-    UPDATE_GROUP_NAME_EVENT: "updateGroupName",
-    MESSAGE_RECEIVED_EVENT: "messageReceived",
-    NEW_CHAT_EVENT: "newChat",
-    SOCKET_ERROR_EVENT: "socketError",
-    STOP_TYPING_EVENT: "stopTyping",
-    TYPING_EVENT: "typing",
-    MESSAGE_DELETE_EVENT: "messageDeleted",
-    SEND_NOTIFICATION_EVENT: "sendNotification",
-    BOOK_NOTIFICATION_EVENT: "bookNotification",
+  CONNECTED_EVENT: "connected",
+  DISCONNECT_EVENT: "disconnect",
+  JOIN_CHAT_EVENT: "joinChat",
+  LEAVE_CHAT_EVENT: "leaveChat",
+  UPDATE_GROUP_NAME_EVENT: "updateGroupName",
+  MESSAGE_RECEIVED_EVENT: "messageReceived",
+  NEW_CHAT_EVENT: "newChat",
+  SOCKET_ERROR_EVENT: "socketError",
+  STOP_TYPING_EVENT: "stopTyping",
+  TYPING_EVENT: "typing",
+  MESSAGE_DELETE_EVENT: "messageDeleted",
+  SEND_NOTIFICATION_EVENT: "sendNotification",
+  BOOK_NOTIFICATION_EVENT: "bookNotification",
 });
 
 export const AvailableChatEvents = Object.values(ChatEventEnum);
